516e989edca567ea97a81e92cfcbb6b61adb2419
bh8bvgtbf6-max <bh8bvgtbf6@privaterelay.appleid.com>
2026-05-17 15:10:35 +0200

    Update control.h

--- src/control/control.h
+++ src/control/control.h
@@ -3,5 +3,6 @@
 #include "control/element/Petelier_Polarity/Petelier_Polarity.h"
 #include "control/element/Sensor_Reading/Sensor_Reading.h"
 #include "control/statemachine/statemachine.h"
+#include "control/element/AirQuality/AirQuality.h"              // FE
 
 void runControlLoop();
