0ade05c6f02f293f968655320765c2fd288ae661
bh8bvgtbf6-max <bh8bvgtbf6@privaterelay.appleid.com>
2026-05-17 14:39:13 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,63 +10,26 @@
 void setup() {
   Serial.begin(115200);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-  pinMode(HeatingRelay, OUTPUT);
-  pinMode(CoolingRelay, OUTPUT);
-  digitalWrite(HeatingRelay, HIGH);
-  digitalWrite(CoolingRelay, HIGH);
-=======
   pinMode(relay1, OUTPUT);
   pinMode(relay2, OUTPUT);
   digitalWrite(relay1, HIGH);
   digitalWrite(relay2, HIGH);
-=======
-  pinMode(HeatingRelay, OUTPUT);
-  pinMode(CoolingRelay, OUTPUT);
->>>>>>> 0522460 (air quality feature)
   pinMode(mq135Pin, INPUT);        // FE
-  digitalWrite(HeatingRelay, HIGH);
-  digitalWrite(CoolingRelay, HIGH);
 
-<<<<<<< HEAD
   flapServo.attach(servoPin);      // FE
   flapServo.write(0);              // Klappe geschlossen; FE
->>>>>>> 85be5bb (Update main.cpp)
-=======
-  ledcSetup(MosfetPetelierCh, 20000, 8);
-  ledcSetup(MosfetFan1Ch, 20000, 8);
-  ledcSetup(MosfetFan2Ch, 20000, 8);
->>>>>>> 0522460 (air quality feature)
 
-  ledcAttachPin(MosfetPetelierPin, MosfetPetelierCh);
-  ledcAttachPin(MosfetFan1Pin,   MosfetFan1Ch);
-  ledcAttachPin(MosfetFan2Pin,   MosfetFan2Ch);
+  ledcSetup(0, 20000, 8);
+  ledcSetup(1, 20000, 8);
+  ledcSetup(2, 20000, 8);
 
-<<<<<<< HEAD
-  ledcAttachPin(MosfetPetelierPin, MosfetPetelierCh);
-  ledcAttachPin(MosfetFan1Pin,   MosfetFan1Ch);
-  ledcAttachPin(MosfetFan2Pin,   MosfetFan2Ch);
-=======
-  ledcWrite(MosfetPetelierCh, 0);
-  ledcWrite(MosfetFan1Ch, 0);
-  ledcWrite(MosfetFan2Ch, 0);
+  ledcAttachPin(mosfetPin, 0);
+  ledcAttachPin(fan1Pin,   1);
+  ledcAttachPin(fan2Pin,   2);
 
-  flapServo.attach(servoPin); // FE 
-  flapServo.write(0);         // Klappe geschlossen; FE
-  delay(1000);   
-
-  mq135Raw = analogRead(mq135Pin);
-  Serial.printf("[mq135] value: %d\n", mq135Raw);
-
-  if (mq135Raw == 0 || mq135Raw >= 4000 ) {
-    Serial.println("ERROR: mq135 not found!");
-  } else 
-  {
-    Serial.println("mq135 ok");
-  } 
->>>>>>> 0522460 (air quality feature)
-
+  ledcWrite(0, 0);
+  ledcWrite(1, 0);
+  ledcWrite(2, 0);
 
   if (!bmp.begin()) {
     Serial.println("ERROR: BMP180 not found!");
